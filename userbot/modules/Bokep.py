17410c666bb2b471538a74f7ffa12a484a2b660e
Babymu21 <99784792+Babymu21@users.noreply.github.com>
2022-04-13 12:49:15 +0700

    Create Bokep.py

---
+++ userbot/modules/Bokep.py
@@ -0,0 +1,42 @@
+# 🍀 © @tofik_dn
+# ⚠️ Do not remove credits
+#😂😂😂😂
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import poci_cmd
+import random
+from userbot import owner
+from telethon.tl.types import InputMessagesFilterVideo
+from telethon.tl.types import InputMessagesFilterVoice
+
+
+@poci_cmd(pattern="bokep$")
+async def _(event):
+    try:
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@F444444444444444", filter=InputMessagesFilterVideo
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(asupannya),
+            caption=f"**Berhasil menemukan Bokep jangan sange kontol**.")
+        
+        await event.delete()
+    except Exception:
+        await event.edit("Tidak bisa menemukan video asupan.")
+
+        
+
+CMD_HELP.update(
+    {
+        "asupan": f"**Plugin : **`bokep`\
+        \n\n  •  **Syntax :** `{cmd}Bokep`\
+        \n  •  **Function : **Untuk Mengirim video Bokep untuk kamu yang sangean.\
+    "
+    }
+)
